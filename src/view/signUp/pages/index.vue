76866cdf681a357376fe861a981302d88c8d3fc7
galaa2011 <zhaopeng7353@126.com>
2018-10-26 14:31:12 +0800

    增加号段177

--- src/view/signUp/pages/index.vue
+++ src/view/signUp/pages/index.vue
@@ -26,7 +26,7 @@ export default {
   },
   computed: {
     state() {
-      return /^1[3|4|5|8][0-9]\d{8}$/.test(this.phone);
+      return /^1[3|4|5|7|8][0-9]\d{8}$/.test(this.phone);
     }
   }
 }
